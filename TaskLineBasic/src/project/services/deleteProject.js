201f2f190b32c6418b898e62a8150991b338c3c0
ghaydarafa <ghayda.rafa@gmail.com>
2025-05-13 16:43:03 +0700

    fix: catch error delete project

--- TaskLineBasic/src/project/services/deleteProject.js
+++ TaskLineBasic/src/project/services/deleteProject.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import tokenManager from '@/commons/utils/token'
 import environment from '@/commons/utils/environment'
 import cleanFormData from '@/commons/utils/cleanFormData'
+import { notifyError } from '@/commons/utils/toaster'
 
 
 const deleteProject = (data = {}) => {
@@ -18,6 +19,10 @@ const deleteProject = (data = {}) => {
 			'Authorization': token,
 			
 		}
-	})} 
+	}).catch((error) => {
+		console.error(error);
+		notifyError(error);
+		throw error;
+	})}
 
 export default deleteProject
