360c4806c6186217768aab9636b7a1d15a6761ae
syadzaarrana <syadzadesvianto@gmail.com>
2025-05-18 22:02:28 +0700

    feat: additional from unreleased ui gen for addedviewcontainer

---
+++ TaskLineWeek5/src/project/components/AddedFormAddProjectMemberForm.jsx
@@ -0,0 +1,107 @@
+/*
+	Generated on 18/05/2025 by UI Generator PRICES-IDE
+	https://amanah.cs.ui.ac.id/research/ifml-regen
+	version 3.10.0
+*/
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { useNavigate, useSearchParams } from "react-router";
+import {
+  Button,
+  Form,
+  SelectionField,
+  MultiSelectionField,
+  InputField,
+  MultiSelectField,
+  RadioInputField,
+  TextAreaField,
+  RichTextField,
+  VisualizationAttr,
+  Spinner,
+  
+  
+} from "@/commons/components";
+import {
+  ALLOWED_PERMISSIONS,
+  findAllowedPermission,
+} from "@/commons/constants/allowedPermission";
+import cleanFormData from "@/commons/utils/cleanFormData";
+import saveProjectMemberProjectWithMembers from '../services/saveProjectMemberProjectWithMembers'
+
+import { notifyError, notifySuccess} from "@/commons/utils/toaster";
+import * as Layouts from "@/commons/layouts";
+
+const AddedFormAddProjectMemberForm = ({ 
+	memberListData
+ }) => {
+  const { 
+    control, 
+    handleSubmit,
+  } = useForm({ defaultValues: memberListData })
+  
+  
+  
+  
+  const navigate = useNavigate()
+  
+  const addMember = (data) => {
+    const cleanData = cleanFormData(data)
+    saveProjectMemberProjectWithMembers({
+      ...cleanData,
+    })
+    .then(({ data: { data } }) => {
+     navigate(`/projects/${memberListData.projectId}`)
+  	notifySuccess(`SaveProjectMember ProjectWithMembers berhasil!`);
+    })
+    .catch((error) => {
+      console.error(error);
+          notifyError(error);
+    });
+  }
+  
+  
+  return (
+	<div>
+	  <Layouts.FormComponentLayout
+		  title="Add Project Member Form" 
+		  onSubmit={handleSubmit(addMember)}
+	
+	    vas={[
+		  ]}
+	
+		  formFields={[
+		  
+	
+		  
+		  <Controller
+		    key="memberId"
+	        name="memberId"
+	        control={control}
+	        render={({ field, fieldState }) => (
+					<SelectionField
+				
+	            label="Member"
+	            options={memberListData}
+				optionKey="memberId"
+	            optionLabel="name"
+	            placeholder="Masukkan member"
+					fieldState={fieldState}
+					defaultValue={memberListData.memberId}
+	            {...field}
+					isRequired={false}
+	          />
+	
+	        )}
+	      />
+		  ]}
+	
+		  itemsEvents={[
+				<Button id="_sXE3cDJDEfCBKLsveFPh7w" key="Add Member" type="submit" variant="primary">Add Member</Button>
+	    ]}
+	  />
+	    
+	</div>
+  )
+}
+
+export default AddedFormAddProjectMemberForm
---
+++ TaskLineWeek5/src/project/containers/AddProjectMemberPage.jsx
@@ -0,0 +1,70 @@
+/*
+	Generated on 18/05/2025 by UI Generator PRICES-IDE
+	https://amanah.cs.ui.ac.id/research/ifml-regen
+	version 3.10.0
+*/
+import React, { useEffect, useState, useContext} from 'react'
+import { Button, Spinner } from "@/commons/components"
+import * as Layouts from '@/commons/layouts';
+import { Link } from "react-router";
+import { useParams } from "@/commons/hooks/useParams"
+import { HeaderContext } from "@/commons/components"
+import { useSearchParams } from "react-router";
+import AddedFormAddProjectMemberForm from '../components/AddedFormAddProjectMemberForm'
+
+import getMemberListData from '../services/getMemberListData'
+const AddProjectMemberPage = props => {
+const { projectId } = useParams()
+
+	const [isLoading, setIsLoading] = useState({
+	addProjectMemberForm: false,
+
+	});
+	const { setTitle } = useContext(HeaderContext);
+
+const [memberListData, setMemberListData] = useState()
+
+useEffect(() => {
+    const fetch = async () => {
+	  setIsLoading(prev => ({...prev, addProjectMemberForm: true}))
+		const { data: memberListDataResponse } = await getMemberListData({ projectId  })
+
+	    setMemberListData(memberListDataResponse.data)
+
+
+	    setIsLoading(prev => ({...prev, addProjectMemberForm: false}))
+    }
+	fetch()
+  }, [])
+
+	
+	useEffect(() => {
+		setTitle("Add Project Member Page")
+	}, []);
+return (
+	<Layouts.ViewContainerLayout
+		buttons={
+			<>
+			<></>
+			</>
+		}
+	>
+<Layouts.FormContainerLayout
+		singularName={"Project"}
+		isLoading={isLoading.addProjectMemberForm}
+	>
+		{memberListData ? 
+		(<>
+		 <AddedFormAddProjectMemberForm
+			{...{ 
+				memberListData
+				}}
+		 /> 
+		</>)  : (<></>)}
+	</Layouts.FormContainerLayout>
+
+	</Layouts.ViewContainerLayout>
+  )
+}
+export default AddProjectMemberPage
+
